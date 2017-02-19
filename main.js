cff1ad21b308a4c2d6cb4ca01e95ccc24d6138b9
arihito <arihito.m@gmail.com>
2017-02-19 17:25:59 +0900

    catch simple flow

--- main.js
+++ main.js
@@ -1,35 +1,32 @@
-// promiseオブジェクトを生成
-var promise = new Promise(
+// Promiseオブジェクトを返す関数
+function asyncPromise() {
 
-  // 非同期処理が終わったら成功時にresolve失敗時にrejectを呼ぶ
-  function(resolve, reject){
+  // promiseオブジェクトを生成
+  return new Promise(
 
-    // 非同期の処理
-    setTimeout(function(){
-      resolve("Hello ");
-    },2000)
+    // 非同期処理が終わったら成功時にresolve失敗時にrejectを呼ぶ
+    function(resolve, reject){
 
-  }
-);
+      // 非同期の処理
+      setTimeout(function(){
+        resolve("Hello ");
+      },2000)
+
+    }
+  );
+}
 
 // 成功/失敗の処理を登録
-promise.then(
+asyncPromise().then(
 
   // onFulfilled resolveが呼び出された成功時
   function(result){
     document.write(result + "World");
-  },
+  }
+).catch(
 
-  // onRejected rejectが呼び出された失敗時
-  function(result){
-    console.log(result);
+  // 失敗だけの処理を登録
+  function(error){
+    console.error(error);
   }
 );
-
-// 失敗だけの処理を登録
-// promise.catch(onRejected);
-
-
-// グローバルオブジェクトの静的な補助メソッド
-// promise.all();
-// promise.resolve();
