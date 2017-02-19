b2403b4c58a5d3b5dfb85acf89d08d16c2ed6626
arihito <arihito.m@gmail.com>
2017-02-19 17:14:49 +0900

    onFulFill flow

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-var p = new Promise(function(fulfill,reject){
-  setTimeout(function(){
-    document.write(fulfill);
-  },3000)
-});
+// promiseオブジェクトを生成
+var promise = new Promise(
 
-p.then(function(result){
-  document.write("World");
-},function(result){
-  console.log(result);
-});
+  // 非同期処理が終わったら成功時にresolve失敗時にrejectを呼ぶ
+  function(resolve, reject){
+
+    // 非同期の処理
+    setTimeout(function(){
+      resolve("Hello ");
+    },2000)
+
+  }
+);
+
+// 成功/失敗の処理を登録
+promise.then(
+
+  // onFulfilled resolveが呼び出された成功時
+  function(result){
+    document.write(result + "World");
+  },
+
+  // onRejected rejectが呼び出された失敗時
+  function(result){
+    console.log(result);
+  }
+);
+
+// 失敗だけの処理を登録
+// promise.catch(onRejected);
+
+
+// グローバルオブジェクトの静的な補助メソッド
+// promise.all();
+// promise.resolve();
